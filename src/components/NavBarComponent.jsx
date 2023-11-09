b155f35a39fe2da007057a7161427e9630228728
mvg.ljp <mvg.ljp@hotmail.com>
2023-11-09 19:03:37 -0300

    entrega1

---
+++ src/components/NavBarComponent.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavBarComponent = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Tienda de bebidas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Productos Artesanales</Nav.Link>
+            <NavDropdown title="Opciones" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">vinos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Licores</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Cervezas</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBarComponent;
